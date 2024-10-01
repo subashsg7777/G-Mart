4fa2466c482bcad739fd508114f6fe4ea9661552
subashsg7777 <subashsg7777@gmail.com>
2024-10-01 17:35:12 +0530

    Added Location Feature
    Derives Location from the user automattically

--- g-mart/src/Navbar.js
+++ g-mart/src/Navbar.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './static/output.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping ,faLocationArrow, faUser} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  // location useStates
+  let [gps,setGps] = useState({lattitude:null,longitude:null});
+    let [error,setError] = useState(null);
+
+    useEffect(()=>{
+      // checking if the browser supports geolocation 
+      if(navigator.geolocation){
+        try{
+          // retriving location 
+          navigator.geolocation.getCurrentPosition((position)=>{
+            setGps({lattitude:position.coords.latitude,longitude:position.coords.longitude});
+          },(error)=>{
+            // display particular error 
+            setError(error);
+          })
+        }
+
+        catch{
+          // displays error message 
+          console.log("error while Getting the location !..");
+        }
+      }
+    });
   return (
   <nav className="bg-gray-200 p-4">
   <div className="container flex items-center ">
@@ -19,18 +42,19 @@ const Navbar = () => {
             placeholder="Search..."
             className="p-2 rounded-l-md border-none focus:outline-none w-[600px]"
           />
-          <button className="bg-blue-600 hover:bg-blue-700 text-white p-2 rounded-r-md">
+          <button className="bg-blue-600 hover:bg-blue-700 text-white p-2 rounded-r-md" onclick={()=>{alert('result not found')}}>
             Search
+          
           </button>
         </div>
         {/* cart icon  */}
-        <button className="flex items-center ml-[100px]">
+        <button className="flex items-center ml-[100px] ">
           <FontAwesomeIcon icon={faCartShopping} className="mr-2" size='xl'/>
         </button>
 
-        <button className='flex items-center ml-[100px] text-white'>
+        <button className='flex items-center  text-white'>
             <FontAwesomeIcon icon={faLocationArrow} className='mr-2' size='xl'/>
-            <p className='whitespace-nowrap'>Chennai-103</p>
+            <p className='whitespace-nowrap pr-[80px]' style={{fontSize:'0.875rem'}}>Lat:{gps.lattitude}&Lon:{gps.longitude}</p>
         </button>
         
         {/* profile icon */}
